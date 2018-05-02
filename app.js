1328aa95db7b441e885aa3bb6cf202c57068d038
ran4214 <ran4214@naver.com>
2018-05-02 22:35:44 +0900

    express 모듈 추가

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-function bjy(){
-  console.log('ㅎㅇ');
-}
+var express = require('express');
+
+var app = express();
+
+app.get('/',function(req,res){
+  res.send('Hello World!');
+});
+
+app.listen(3000,function(){
+  console.log('Server On!');
+});
