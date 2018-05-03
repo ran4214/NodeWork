970fae74404323c211bb79b3287ccff649fecae8
배진영 <ran4214@naver.com>
2018-05-03 10:21:58 +0900

    index.html 파일 및 get 메소드 추가

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var express = require('express');
 
 var app = express();
 
-app.get('/',function(req,res){
+app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000,function(){
+app.get('/detail', function(req, res) {
+  res.send('Hello World! /detail');
+});
+
+app.listen(3000, function() {
   console.log('Server On!');
 });
